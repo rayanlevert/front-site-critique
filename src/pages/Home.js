69d1e9bc34d6a95644c9cb754a297a70003d0f78
Boudin Joey <boudin.joey.pro@gmail.com>
2021-02-24 00:02:33 +0100

    Le saint Graal il manque encore la partie bottom-right

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import logo from '../logo.svg';
 import HomeIndex from '../components/home/HomeIndex';
+
 export default function Home() {
 
   const state = useSelector(state => state);
